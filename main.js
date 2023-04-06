85bbb1d975e95e2cc4afa705b025b16fa005b2fb
vukpop <127890473+vukpop@users.noreply.github.com>
2023-04-06 20:35:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const openInstagram = () => {
+    window.open("https://www.instagram.com/popoviccvuk/");
+}
+const openLinkdln = () => {
+    window.open("https://www.linkedin.com/in/vuk-popovi%C4%87-371a85270/");
+}
